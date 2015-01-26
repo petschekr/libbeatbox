59611606553a245147891cb30d1f0b05ede2d38c
Ryan Petschek <petschekr@gmail.com>
2015-01-26 17:28:51 -0500

    Initial commit with login

---
+++ main.ts
@@ -0,0 +1,43 @@
+var Bluebird = require("bluebird");
+var PlayMusic = require("playmusic");
+
+function noErrorPromisifier(originalMethod) {
+	return function promisified() {
+		var args = [].slice.call(arguments);
+		var self = this;
+		return new Promise(function (resolve,reject) {
+			args.push(resolve); 
+			originalMethod.apply(self,args);
+		});
+	};
+}
+
+interface QueueItem {
+	id: {
+		track: string;
+		album: string;
+		artist: string;
+	};
+	title: string;
+	album: string;
+	artist: string;
+	genre?: string;
+	info?: {
+		duration: number;
+		formattedDuration: string;
+		year?: number;
+		trackNumber?: number;
+	}
+}
+
+class Beatbox {
+	private pm: any;
+	public queue: QueueItem[] = [];
+
+	constructor() {
+		this.pm = Promise.promisifyAll(new PlayMusic(), {promisifier: noErrorPromisifier});
+	}
+	login(email: string, password: string, callback?: (err: Error) => any) {
+		return pm.initAsync({"email": email, "password": password}).nodeify(callback);
+	}
+}
